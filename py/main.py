68262c45e84515e3b2e8e558b1be5223572466b3
david-inf <david.nardi1@hotmail.com>
2024-06-04 09:45:15 +0200

    Circular cities generator added

--- py/main.py
+++ py/main.py
@@ -3,13 +3,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tsp import generate_cities
+from tsp import generate_cities, circle_cities
 
 # %% first try
 
-mat1 = generate_cities(5)
+D1, C1 = generate_cities(5)
 
-plt.scatter(mat1[:,0], mat1[:,1])
+plt.scatter(C1[:,0], C1[:,1])
 plt.show()
 
+# %% second try
 
+D2, C2, C2pol = circle_cities(50)
+
+plt.scatter(C2[:,0], C2[:,1])
+plt.xlim((-6,6))
+plt.ylim((-6,6))
+plt.show()
+
+plt.polar(C2pol[:,0], C2pol[:,1])
+plt.show()
--- py/tsp.py
+++ py/tsp.py
@@ -11,14 +11,40 @@ def generate_cities(ncity, l=10, seed=42):
     # cities coordinates c_i=(x,y)
     C = rng.integers(0, l, ncity * 2).reshape((-1, 2))
 
-    # empty distance matrix
+    # empty distance matrix D=(d_ij)
     D = np.empty((ncity, ncity))
-    ## check 0 diagonal ##
 
     # compute distances
     for i in range(ncity):  # take c_i
         for j in range(ncity):  # compute d_ij
             D[i,j] = la.norm(C[i,:] - C[j,:])
 
-    return D
+    return D, C
 
+
+def circle_cities(ncity, r=5):
+
+    # delta between each city in polar coordinates
+    theta = 2 * np.pi / ncity
+
+    # cities polar coordinates (theta, rho)
+    Cpol = np.zeros((ncity, 2))
+    Cpol[0,:] = np.array([0, r])  # first city
+
+    for i in range(ncity-1):
+        Cpol[i+1,:] = np.array([Cpol[i,0] + theta, r])
+
+    # cities cartesian coordinates (x, y)
+    Ccart = np.zeros((ncity, 2))
+    for i in range(ncity):
+        Ccart[i,:] = r * np.array([np.cos(Cpol[i,0]), np.sin(Cpol[i,0])])
+
+    # empty distance matrix D=(d_ij)
+    D = np.empty((ncity, ncity))
+    
+    # compute distances
+    for i in range(ncity):
+        for j in range(ncity):
+            D[i,j] = la.norm(Ccart[i,:] - Ccart[j,:])
+
+    return D, Ccart, Cpol
